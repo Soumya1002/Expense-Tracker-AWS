20a25f131391a4309c674b55d7774f2f8ae30632
Soumya1002 <soumyasenthil1002@gmail.com>
2024-02-24 08:55:03 +0530

    db changes

--- util/database.js
+++ util/database.js
@@ -2,9 +2,9 @@ const Sequelize = require('sequelize')
 const dotenv = require('dotenv')
 dotenv.config()
 
-const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USERNAME, process.env.DATABASE_PASSWORD, {
-    dialect: process.env.DATABASE_DIALECT,
-    host: process.env.DATABASE_HOST,
+const sequelize = new Sequelize('expense', 'root', 'Soumya*123', {
+    dialect: 'mysql',
+    host: 'localhost',
     logging: false
 });
 
